7b1a3b0a59a7a7d697b54d5a8010a8264353e6a8
stepdugas <131499172+stepdugas@users.noreply.github.com>
2025-04-08 11:36:39 -0400

    Organized project structure

---
+++ New Group/Stacked/StackedApp.swift
@@ -0,0 +1,17 @@
+//
+//  StackedApp.swift
+//  Stacked
+//
+//  Created by Stephanie Dugas on 4/8/25.
+//
+
+import SwiftUI
+
+@main
+struct StackedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
